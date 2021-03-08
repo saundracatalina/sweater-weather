bbfecbdd40ee1f6961c1ffa4e82748b84bb76b4e
Saundra <saundracatalina@gmail.com>
2021-03-07 18:58:58 -0700

    Refactor tests to better describe what the methods are doing

--- spec/api/v1/services/weather_service_spec.rb
+++ spec/api/v1/services/weather_service_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
 describe 'WeatherService' do
-  it '.weather_details' do
-    lat = 39.738453
-    lon = -104.984853
-    data = WeatherService.weather_details(lat, lon)
+  describe '.weather_details' do
+    it 'can show the current, hourly, and daily weather details for a given lat/lon' do
+      lat = 39.738453
+      lon = -104.984853
+      data = WeatherService.weather_details(lat, lon)
 
-    expect(data[:current]).to be_a(Hash)
-    expect(data[:current][:temp]).to be_a(Float)
-    expect(data[:current][:weather][0][:description]).to be_a(String)
+      expect(data[:current]).to be_a(Hash)
+      expect(data[:current][:temp]).to be_a(Float)
+      expect(data[:current][:weather][0][:description]).to be_a(String)
 
-    expect(data[:hourly]).to be_an(Array)
-    expect(data[:hourly][0][:temp]).to be_a(Numeric)
-    expect(data[:hourly][0][:weather]).to be_an(Array)
-    expect(data[:hourly][0][:weather][0][:description]).to be_a(String)
+      expect(data[:hourly]).to be_an(Array)
+      expect(data[:hourly][0][:temp]).to be_a(Numeric)
+      expect(data[:hourly][0][:weather]).to be_an(Array)
+      expect(data[:hourly][0][:weather][0][:description]).to be_a(String)
 
-    expect(data[:daily]).to be_an(Array)
-    expect(data[:daily][0][:sunrise]).to be_a(Numeric)
-    expect(data[:daily][0][:feels_like]).to be_a(Hash)
+      expect(data[:daily]).to be_an(Array)
+      expect(data[:daily][0][:sunrise]).to be_a(Numeric)
+      expect(data[:daily][0][:feels_like]).to be_a(Hash)
+    end
   end
 end
