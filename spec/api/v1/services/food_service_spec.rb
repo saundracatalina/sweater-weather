377f0d5460d0b76ed47be40c00e69432a815a915
Saundra <saundracatalina@gmail.com>
2021-03-08 10:05:41 -0700

    Add more robust test

--- spec/api/v1/services/food_service_spec.rb
+++ spec/api/v1/services/food_service_spec.rb
@@ -6,8 +6,16 @@ describe 'FoodService' do
       location = "Pueblo,CO"
       food = "hamburger"
       data = FoodService.restaurant_details(location, food)
-      require "pry"; binding.pry
-  
+
+      expect(data).to be_a(Hash)
+      expect(data).to have_key(:businesses)
+      expect(data[:businesses]).to be_an(Array)
+      expect(data[:businesses].count).to eq(1)
+      expect(data[:businesses][0]).to be_a(Hash)
+      expect(data[:businesses][0]).to have_key(:name)
+      expect(data[:businesses][0][:name]).to be_a(String)
+      expect(data[:businesses][0]).to have_key(:is_closed)
+      expect(data[:businesses][0][:is_closed]).to eq(false)
     end
   end
 end
