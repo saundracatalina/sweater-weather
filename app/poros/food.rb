acf2c4283f192d2aa89a16fe136fed0fd76a8ebb
Saundra <saundracatalina@gmail.com>
2021-03-08 10:45:10 -0700

    Add Food PORO with just name and address returned

---
+++ app/poros/food.rb
@@ -0,0 +1,9 @@
+class Food
+  attr_reader :name,
+              :address
+
+  def initialize(data)
+    @name = data[:businesses][0][:name]
+    @address = data[:businesses][0][:location][:display_address].join(" ")
+  end
+end
