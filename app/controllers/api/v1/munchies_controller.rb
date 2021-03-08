8f00332d72d723ada7e37ddc9ac777371bd6446c
Saundra <saundracatalina@gmail.com>
2021-03-08 10:22:06 -0700

    Add call to FoodService to test connection from controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
+    weather = ForecastFacade.forecast_details(params[:destination])
+    x = FoodService.restaurant_details(params[:destination], params[:food])
 
+    # call serializer here that combines FoodFacade info
   end
 end
