65d11cb3532ebb454dd340806ac84119cefebbe5
Saundra <saundracatalina@gmail.com>
2021-03-09 15:38:00 -0700

    Add sad path test for missing email

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -24,4 +24,19 @@ describe "a FE request cycle for a user logging in" do
     expect(data[:attributes][:api_key]).to be_a(String)
     expect(data[:attributes][:api_key]).to eq(user.api_key)
   end
+  it "gives a 400 with generic error message if email is missing" do
+    user = create(:user)
+    params = {
+      'password' => user.password
+    }
+    headers = { 'CONTENT_TYPE': 'application/json', 'ACCEPT': 'application/json' }
+
+    post '/api/v1/sessions', headers: headers, params: JSON.generate(params)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(json[:error]).to eq("Incorrect login credentials, please try again")
+  end
 end
