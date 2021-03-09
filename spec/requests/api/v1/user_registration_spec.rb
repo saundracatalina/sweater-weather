165153f2b10fe6fdc3be626348ee3c822b34dc97
Saundra <saundracatalina@gmail.com>
2021-03-09 14:41:50 -0700

    Add sad path test

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -7,26 +7,39 @@ describe "a FE request cycle for registering a user" do
         password: 'password',
         password_confirmation: 'password'
       }
+    headers = { 'CONTENT_TYPE': 'application/json', 'ACCEPT': 'application/json' }
 
-      post '/api/v1/users', params: params
+    post '/api/v1/users', headers: headers, params: JSON.generate(params)
 
-      expect(User.all.count).to eq(1)
-      user = User.last
-      expect(response).to be_successful
-      expect(response.status).to eq(201)
-      expect(response.content_type).to eq('application/json')
+    expect(User.all.count).to eq(1)
+    user = User.last
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(response.content_type).to eq('application/json')
 
-      json = JSON.parse(response.body, symbolize_names: true)
-      data = json[:data]
-      attributes = data[:attributes]
-      expect(json).to_not have_key(:error)
-      expect(json).to have_key(:data)
-      expect(data[:type]).to eq('users')
-      expect(data[:id]).to eq(user.id.to_s)
-      expect(attributes).to be_a(Hash)
-      expect(attributes[:email]).to eq(user.email)
-      expect(attributes[:api_key]).to eq(user.api_key)
-      expect(attributes).to_not have_key(:password)
-      expect(attributes).to_not have_key(:password_confirmation)
+    json = JSON.parse(response.body, symbolize_names: true)
+    data = json[:data]
+    attributes = data[:attributes]
+    expect(json).to_not have_key(:error)
+    expect(json).to have_key(:data)
+    expect(data[:type]).to eq('users')
+    expect(data[:id]).to eq(user.id.to_s)
+    expect(attributes).to be_a(Hash)
+    expect(attributes[:email]).to eq(user.email)
+    expect(attributes[:api_key]).to eq(user.api_key)
+    expect(attributes).to_not have_key(:password)
+    expect(attributes).to_not have_key(:password_confirmation)
+  end
+  it "cannot register a user if no email, password, or password confirmation is sent" do
+    params = {
+        email:  'user@example.com',
+        password_confirmation: 'password'
+      }
+    headers = { 'CONTENT_TYPE': 'application/json', 'ACCEPT': 'application/json' }
+
+    post '/api/v1/users', headers: headers, params: JSON.generate(params)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(401)
   end
 end
