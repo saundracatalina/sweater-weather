a87a57c1aaef5f7e139b6b298aff4436950f6b9c
Saundra <saundracatalina@gmail.com>
2021-03-09 15:52:50 -0700

    Refactor create action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: session_params[:email])
-    if user && user.authenticate(session_params[:password])
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       render json: UsersSerializer.new(user), status: :ok
     else
       render json: {error: 'Incorrect login credentials, please try again'}, status: :bad_request
     end
-  end
-
-  private
-
-  def session_params
-    JSON.parse(request.body.read, symbolize_names: true)
   end
 end
