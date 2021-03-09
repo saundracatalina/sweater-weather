2425f4c44f152cb9a613fd295ceca78e0de40791
Saundra <saundracatalina@gmail.com>
2021-03-08 20:12:34 -0700

    Add environment variables to filter_sensitive_data VCR config block

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -78,4 +78,9 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.filter_sensitive_data('MAP_API_KEY') { ENV['MAP_API_KEY'] }
+  config.filter_sensitive_data('WEATHER_API_KEY') { ENV['WEATHER_API_KEY'] }
+  config.filter_sensitive_data('PHOTO_API_KEY') { ENV['PHOTO_API_KEY'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
 end
