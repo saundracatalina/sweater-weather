8e5f9fdd835e78cc2df252bab1a85e5b36d722b3
Saundra <saundracatalina@gmail.com>
2021-03-09 12:31:44 -0700

    Add User model and set_api_key method

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  before_create :set_api_key
+  validates :email,
+            presence: true,
+            uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
+  has_secure_password
+
+  private
+
+  def set_api_key
+    self.api_key = SecureRandom.uuid
+  end
+end
