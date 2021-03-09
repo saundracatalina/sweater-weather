7ba3d87f4fdab2445a0251963d7c3f62f5fd1223
Saundra <saundracatalina@gmail.com>
2021-03-09 15:40:49 -0700

    Add edge case tests for a mispelled email or password

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -54,4 +54,36 @@ describe "a FE request cycle for a user logging in" do
     expect(response.status).to eq(400)
     expect(json[:error]).to eq("Incorrect login credentials, please try again")
   end
+  it "gives a 400 with generic error message if password is mispelled" do
+    user = create(:user)
+    params = {
+      'email' => user.email,
+      'password' => "not_a_real_password"
+    }
+    headers = { 'CONTENT_TYPE': 'application/json', 'ACCEPT': 'application/json' }
+
+    post '/api/v1/sessions', headers: headers, params: JSON.generate(params)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(json[:error]).to eq("Incorrect login credentials, please try again")
+  end
+  it "gives a 400 with generic error message if email is mispelled or doesn't exist" do
+    user = create(:user)
+    params = {
+      'email' => "not_a_real_email@example.com",
+      'password' => user.password
+    }
+    headers = { 'CONTENT_TYPE': 'application/json', 'ACCEPT': 'application/json' }
+
+    post '/api/v1/sessions', headers: headers, params: JSON.generate(params)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(json[:error]).to eq("Incorrect login credentials, please try again")
+  end
 end
