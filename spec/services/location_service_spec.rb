dfdcc8c816008cc2cb5349f0264bd04aebe6afcc
Saundra <saundracatalina@gmail.com>
2021-03-08 20:24:34 -0700

    Add VCR to test

--- spec/services/location_service_spec.rb
+++ spec/services/location_service_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 describe 'LocationService' do
   describe '.location_details' do
     it 'returns JSON that includes the lat/lon of a given city' do
-      location = "Denver,CO"
-      data = LocationService.location_details(location)
-      results = data[:results]
+      VCR.use_cassette('location_specs_by_city_and_state') do
+        location = "Denver,CO"
+        data = LocationService.location_details(location)
+        results = data[:results]
 
-      expect(results).to be_an(Array)
-      expect(results[0][:locations]).to be_an(Array)
-      expect(results[0][:locations][0][:latLng]).to be_a(Hash)
-      expect(results[0][:locations][0][:latLng][:lat]).to be_a(Float)
-      expect(results[0][:locations][0][:latLng][:lng]).to be_a(Float)
+        expect(results).to be_an(Array)
+        expect(results[0][:locations]).to be_an(Array)
+        expect(results[0][:locations][0][:latLng]).to be_a(Hash)
+        expect(results[0][:locations][0][:latLng][:lat]).to be_a(Float)
+        expect(results[0][:locations][0][:latLng][:lng]).to be_a(Float)
+      end 
     end
   end
 end
